cc4cc4f7a8b6ff45fc0555b6457b2b21186c89c1
Jorge Gaitan <info@dinnovos.com>
2016-01-28 17:10:08 -0430

    Primer Commit v1.0.0

---
+++ src/Kodazzi/Installer/Installer.php
@@ -0,0 +1,44 @@
+<?php
+ /**
+ * This file is part of the Kodazzi Framework.
+ *
+ * (c) Jorge Gaitan <info@kodazzi.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Kodazzi\Installers;
+
+use Composer\IO\IOInterface;
+use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+
+class Installer extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        if (!preg_match('/^(kodazzi)/i', $package->getPrettyName()))
+        {
+            throw new \InvalidArgumentException(
+                'Unable to install template, phpdocumentor templates '
+                .'should always start their package name with '
+                .'"kodazzi/"'
+            );
+        }
+
+        return 'system/bundles/'.$package->getPrettyName();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return 'kodazzi-bundle' === $packageType;
+    }
+} 
---
+++ src/Kodazzi/Installer/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+ /**
+ * This file is part of the Kodazzi Framework.
+ *
+ * (c) Jorge Gaitan <info@kodazzi.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Kodazzi\Installer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
