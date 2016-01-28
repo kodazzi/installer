efaceaad24a2d9e0d2e532a2f52887d0dbdfe743
Jorge Gaitan <info@dinnovos.com>
2016-01-28 17:33:47 -0430

    Correccion en el namespace Kodazzi\Installer

--- src/Kodazzi/Installer/Installer.php
+++ src/Kodazzi/Installer/Installer.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Kodazzi\Installers;
+namespace Kodazzi\Installer;
 
 use Composer\IO\IOInterface;
 use Composer\Installer\LibraryInstaller;
